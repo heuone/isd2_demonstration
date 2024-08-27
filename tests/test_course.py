8aa2eec53d8db8eec657f970adc7540bc45dbced
heuone castillo <hcastillo2@academic.rrc.ca>
2024-08-27 15:02:19 -0500

    Added unittest

--- tests/test_course.py
+++ tests/test_course.py
@@ -8,3 +8,19 @@ the following command:
     python -m unittest tests/test_course.py
 """
 import unittest
+
+from course.course import Course
+from department.department import Department
+
+class TestClient(unittest.TestCase):
+
+    def test_init_valid(self):
+        course = Course("Intermediate Software Development", Department.COMPUTER_SCIENCE,6)
+
+    def test_init_invalid_name_raises_exception(self):
+        with self.assertRaises(ValueError):
+            course = Course(" ", Department.COMPUTER_SCIENCE,6)
+    
+    def test_init_invalid_department(self):
+        with self.assertRaises(ValueError):
+            course = Course("Intermediate Software Development", "INVALID", 6)
